c54c64481ae12a5989e78c3677813de683ea26d9
root <root@unittest01.core.irknet.lan>
2014-12-25 03:09:51 +0000

    fix other errors

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ mkdir -p /usr/lib/selenium/
 cp selenium-server-standalone.jar /usr/lib/selenium/ 
 cp selenium-server /etc/init.d/selenium-server
 chmod a+x /etc/init.d/selenium-server
+mkdir -p /var/log/selenium/
+update-rc.d -f selenium-server defaults
+update-rc.d selenium-server enable 
